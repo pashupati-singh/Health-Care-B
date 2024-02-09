1de33f191cb466cf31d4b6023f07edea44d0f9c7
pashupati-singh <119448686+pashupati-singh@users.noreply.github.com>
2024-02-09 15:30:41 +0530

    update

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 
 const mongoose = require('mongoose')
 require("dotenv").config()
-const connect = mongoose.connect(process.env.URl)
+const connect = mongoose.connect(process.env.URL)
 
 module.exports = {connect}
